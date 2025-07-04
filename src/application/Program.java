10588fe930b727327790f685aba5b8aa7ebd6b34
dvd20 <dvd2.0@live.com>
2025-07-04 11:28:17 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,6 +13,10 @@ public class Program {
         Department obj = new Department("Books", 1);
 
         System.out.println(obj);
+
+        Seller seller = new Seller(1, "Carlos", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
