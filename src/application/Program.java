4e4ad15a619f850651ab4c9b9fc5c83f16c94f60
dvd20 <dvd2.0@live.com>
2025-07-07 11:27:10 -0300

    findByDeparment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -16,7 +17,24 @@ public class Program {
 
         Seller seller = sellerDao.findById(3);
 
+        System.out.println("TEST #1 : find by Id: ");
+
         System.out.println(seller);
+        
+        System.out.println();
+
+        System.out.println("TEST #2 : find by Department");
+
+        Department department = new Department(null, 2);
+
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+
+
+
 
     }
 }
